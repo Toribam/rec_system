858d05bb76b4728aec1b9f702038b1f127009716
Tori <csh6484@naver.com>
2024-10-02 15:54:18 +0900

    Python function
    *updated
    1. Cosine similarity
    2. Euclidean distance
    
    *to-do
    calc all individuals

--- r_scr/1_testing.R
+++ r_scr/1_testing.R
@@ -6,3 +6,29 @@ df <- as.data.frame(df)
 rownames(df) <- df[[1]]
 
 df <- df[-1]
+
+### correlation coefficient
+
+result_cor1 <- cos(df)
+
+plot(df)
+
+
+#install.packages("PerformanceAnalytics")
+library(PerformanceAnalytics)
+
+chart.Correlation(df, histogram = TRUE, method = "pearson")
+
+
+#install.packages("psych")
+library(psych)
+
+corPlot(df, cex = 1.2)
+
+
+
+### cosine similarity
+
+install.packages("lsa", dependencies = TRUE)
+library(lsa)
+
